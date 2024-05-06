25007972cd8d41cf8c4fde2a1c60a44092e2ab7d
Devinder Manhas <tonistarc100000@gmail.com>
2024-05-06 13:44:29 +0530

    PROJECT 50%

--- Other_pages/javascript.js
+++ Other_pages/javascript.js
@@ -1,3 +1,12 @@
+document.addEventListener('click', function(event) {
+    var searchbar = document.getElementById('searchbar');
+    var isClickInsideSearchBar = searchbar.contains(event.target);
+    
+    if (!isClickInsideSearchBar) {
+        searchbar.classList.remove('expanded');
+    }
+});
+
 function toggleWidth() {
     var searchbar = document.getElementById('searchbar');
     searchbar.classList.toggle('expanded');
